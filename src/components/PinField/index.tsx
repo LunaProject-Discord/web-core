3d03e228f68f3b68172228641f60cd9bb9d63b20
Aoichaan0513 <aoichaan0513@gmail.com>
2023-06-09 21:13:15 +0900

    [Fix] 依存関係として追加した際にエラーが出る問題を修正

--- src/components/PinField/index.tsx
+++ src/components/PinField/index.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { Box, styled } from '@mui/material';
-import React, { ChangeEvent, ClipboardEvent, KeyboardEvent, useState } from 'react';
+import React, { ClipboardEvent, KeyboardEvent, useState } from 'react';
 import { ItemDisabledProps, ItemVariableProps } from '../SectionItems';
 
 const IGNORED_META_KEYS = ['Control', 'Alt', 'Meta', 'Tab', 'CapsLock', 'Shift', 'Enter'];
@@ -103,9 +103,6 @@ export const PinField = ({ value, setValue, length, pattern, capitalize, disable
         }
     };
 
-    const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    };
-
     const handleInputPaste = (e: ClipboardEvent<HTMLInputElement>) => {
         const parentElement = e.currentTarget.parentElement;
 
@@ -135,7 +132,6 @@ export const PinField = ({ value, setValue, length, pattern, capitalize, disable
                 <PinFieldInput
                     key={i}
                     value={values[i]}
-                    onChange={(e) => handleInputChange(e)}
                     onKeyDown={(e) => handleInputKeyDown(e, i)}
                     onPaste={(e) => handleInputPaste(e)}
                     type="text"
