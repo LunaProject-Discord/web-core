3dea9e079f8656cd7f5d121b4decfeabcc1bc473
Aoichaan0513 <aoichaan0513@gmail.com>
2023-06-10 17:59:34 +0900

    [Fix] 見出しの出力を修正

--- src/markdown/rules/heading.tsx
+++ src/markdown/rules/heading.tsx
@@ -9,7 +9,7 @@ export const heading: MarkdownRule = {
     order: defaultRules.heading.order,
     match: blockRegex(HEADING_RE),
     parse: (capture, parse, state) => ({
-        level: capture[1].length,
+        level: capture[0].length,
         content: parseInline(parse, capture[2].trim(), state)
     }),
     react: (node, output, state) => {
@@ -21,6 +21,8 @@ export const heading: MarkdownRule = {
                 return (<HeadingTwo key={state.key}>{children}</HeadingTwo>);
             case 3:
                 return (<HeadingThree key={state.key}>{children}</HeadingThree>);
+            default:
+                return children;
         }
     }
 };
