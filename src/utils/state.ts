4f8a9c4318e13b1e5302cb3107c990a581724d40
Aoichaan0513 <aoichaan0513@gmail.com>
2023-10-10 00:45:45 +0900

    [Fix] useResettableState の initialState の型を修正

--- src/utils/state.ts
+++ src/utils/state.ts
@@ -1,6 +1,6 @@
 import { Dispatch, DispatchWithoutAction, SetStateAction, useState } from 'react';
 
-export const useResettableState = <T>(initialState: T): [T, Dispatch<SetStateAction<T>>, DispatchWithoutAction] => {
+export const useResettableState = <T>(initialState: T | (() => T)): [T, Dispatch<SetStateAction<T>>, DispatchWithoutAction] => {
     const [state, setState] = useState(initialState);
     const resetState = () => setState(initialState);
     return [state, setState, resetState];
