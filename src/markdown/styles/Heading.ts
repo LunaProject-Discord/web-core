9f553da2170db821478b3c4a1be3c7335e76dd65
Aoichaan0513 <aoichaan0513@gmail.com>
2024-02-08 22:34:35 +0900

    [Update] デザインの調整

--- src/markdown/styles/Heading.ts
+++ src/markdown/styles/Heading.ts
@@ -2,9 +2,12 @@ import styled from '@emotion/styled';
 
 export const HeadingOne = styled('h1')({
     margin: '16px 0 8px',
-    fontSize: '1.rem',
+    fontSize: '1.5rem',
     fontWeight: 700,
-    lineHeight: '1.375em'
+    lineHeight: '1.375em',
+    '&:first-child': {
+        marginTop: 8
+    }
 });
 
 export const HeadingTwo = styled(HeadingOne.withComponent('h2'))({
@@ -12,5 +15,8 @@ export const HeadingTwo = styled(HeadingOne.withComponent('h2'))({
 });
 
 export const HeadingThree = styled(HeadingOne.withComponent('h3'))({
-    fontSize: '1rem'
+    fontSize: '1rem',
+    '&:first-child': {
+        marginTop: 4
+    }
 });
