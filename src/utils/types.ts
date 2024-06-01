fdd6af9f1fe0eaba3e432d18500a7f1d77e27b57
Aoichaan0513 <aoichaan0513@gmail.com>
2024-06-01 21:59:50 +0900

    [Update] types.ts

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -5,3 +5,7 @@ export type DeepRequired<T> = {
 export type DeepPartial<T> = {
     [P in keyof T]?: DeepPartial<T[P]>;
 };
+
+export type SomeRequired<T, K extends keyof T> = Omit<T, K> & Required<Pick<T, K>>;
+
+export type SomePartial<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
