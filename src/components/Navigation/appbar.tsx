6dc3402e358e8b6372b7288f73792ebb4a826cdd
Aoichaan0513 <aoichaan0513@gmail.com>
2024-09-01 17:22:17 +0900

    [Update] CSS Variables を用いたスタイルの定義 #3

--- src/components/Navigation/appbar.tsx
+++ src/components/Navigation/appbar.tsx
@@ -80,16 +80,16 @@ export const NavigationToolbarItem = (
                 backgroundColor: (theme.vars || theme).palette.action.selected,
                 [`&.${buttonClasses.focusVisible}`]: {
                     backgroundColor: theme.vars
-                        ? `rgba(${theme.vars.palette.action.selected} / calc(${theme.vars.palette.action.selectedOpacity} + ${theme.vars.palette.action.focusOpacity}))`
+                        ? `rgba(${theme.vars.palette.action.selectedChannel} / calc(${theme.vars.palette.action.selectedOpacity} + ${theme.vars.palette.action.focusOpacity}))`
                         : alpha(theme.palette.action.selected, theme.palette.action.selectedOpacity + theme.palette.action.focusOpacity)
                 },
                 '&:hover': {
                     backgroundColor: theme.vars
-                        ? `rgba(${theme.vars.palette.action.selected} / calc(${theme.vars.palette.action.selectedOpacity} + ${theme.vars.palette.action.hoverOpacity}))`
+                        ? `rgba(${theme.vars.palette.action.selectedChannel} / calc(${theme.vars.palette.action.selectedOpacity} + ${theme.vars.palette.action.hoverOpacity}))`
                         : alpha(theme.palette.action.selected, theme.palette.action.selectedOpacity + theme.palette.action.hoverOpacity),
                     '@media (hover: none)': {
                         backgroundColor: theme.vars
-                            ? `rgba(${theme.vars.palette.action.selected} / ${theme.vars.palette.action.selectedOpacity})`
+                            ? `rgba(${theme.vars.palette.action.selectedChannel} / ${theme.vars.palette.action.selectedOpacity})`
                             : alpha(theme.palette.action.selected, theme.palette.action.selectedOpacity)
                     }
                 }
